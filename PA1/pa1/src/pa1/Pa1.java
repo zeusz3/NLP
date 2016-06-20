3e1f6ccb8e3ca0633140f62b2bb41280287dad5c
zeusz3 <>
2016-06-20 14:42:31 +0300

    CHANGES: -use of TreeMap, outputs gene_rare.train

--- PA1/pa1/src/pa1/Pa1.java
+++ PA1/pa1/src/pa1/Pa1.java
@@ -7,6 +7,7 @@ package pa1;
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,44 +15,34 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author harab
  */
 public class Pa1 {
+    
+    private static TreeMap<String, Integer> tm = new TreeMap();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         String oldText = "";
-        Charset charset = Charset.forName("US-ASCII");
-        try( BufferedReader reader = Files.newBufferedReader(Paths.get("b:/coursera/NLP/PA1/h1-p/gene.train"), charset)) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                oldText += line + "\r\n";
-                System.out.println(line);
-            }
-            reader.close();
-        } catch (IOException x) {
-            System.err.format("IOException: %s%n", x);
-        }
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get("b:/coursera/NLP/PA1/h1-p/gene.counts"), charset)) {
-            System.out.println("counts");
-            String line = null;
+        Charset charset = Charset.forName("UTF-8");
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("../h1-p/gene.counts"), charset)) {
+            String line = "";
             while ((line = reader.readLine()) != null) {
                 if(!line.isEmpty()) {
                     if(line.contains("WORDTAG")) {
-                        System.out.print(line + " ");
+                        System.out.println(line + " ");
                         String[] words = line.split(" ");
                         int wordCount = Integer.valueOf(words[0]);
-                        if(wordCount < 5) {
-                            System.out.println("Old: " + words[3] + " Count: " + words[0]);
-                            oldText.replaceAll(words[3], "_RARE_");
-                        }
+                        tm.put(words[3], wordCount);
                     }
                 }
             }
@@ -59,14 +50,26 @@ public class Pa1 {
         } catch (IOException x) {
             System.err.format("IOException: %s%n", x);
         }
-        FileWriter writer;
-        try {
-            writer = new FileWriter("b:/coursera/NLP/PA1/h1-p/gene_rare.counts");
-            writer.write(oldText);
-        } catch (IOException ex) {
-            Logger.getLogger(Pa1.class.getName()).log(Level.SEVERE, null, ex);
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("../h1-p/gene.train"), charset)) {
+            String line = null;
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get("../h1-p/gene_rare.train"), charset);
+            int i = 0;
+            while ((line = reader.readLine()) != null) {
+                if(!line.isEmpty()) {
+                    String words[] = line.split(" ");
+                    System.out.println(words[0]);
+                    if(tm.get(words[0]) < 5) {
+                        line = line.replace(words[0], "_RARE_");
+                    }
+                }
+                writer.write(line, 0, line.length());
+                writer.newLine();
+                System.out.println(line);
+            }
+            reader.close();
+            writer.close();
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
         }
-             
     }
-    
 }
